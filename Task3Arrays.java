8cfdcda45f2a2a7d4a7de699e7d62afb72b7a790
coffeacc10@gmail.com <1234567q>
2018-01-26 18:05:50 +0200

    Added Array to Task3

--- Task3Arrays.java
+++ Task3Arrays.java
@@ -10,14 +10,10 @@ public class Task3Arrays {
         System.out.println("Please enter first number");
         int m=0;
         try {
-
             int n = scan.nextInt();
             if(n>3) {
                 m = n;
-
                 return m;
-
-
             }
             else {
                 System.out.println("Bad Ass!!! Now Let's go from the Begining");
@@ -27,7 +23,7 @@ public class Task3Arrays {
 
         catch (InputMismatchException e) {
 
-
+System.out.println("OMG you gonna pay for this\n");
             getSizeOfArray();
 
         }
@@ -40,14 +36,21 @@ return m;
 
 int a = getSizeOfArray();
 //  System.out.println(a);
-        System.out.println();
-        System.out.println("Here your first Array list");
-        System.out.println();
+        System.out.println("\nHere your first Array list\n");
         int[] array = new int[a];
+        int evenNumbersCount=0;
         for(int i=0;i<array.length;i++){
             array[i]=(int)(Math.random()*a);
             System.out.print(array[i]+" ");
+            if(array[i]%2==0){
+                evenNumbersCount++;
+            }
+            else{};
         }
+        System.out.println(evenNumbersCount);
+
+
+
 
     }
 }
